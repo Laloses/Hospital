9ef0a68fbd7852df9f2e73ea9aa89be65b7afd36
laloses <laloses_eduardo@hotmail.com>
2019-10-11 23:06:56 -0500

    ui login completa
    Falta hacer codigo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,15 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_verRegistros_clicked()
+{
+    //Página de login
+    ui->stackedWidget_registros->setCurrentIndex(1);
+}
+
+void MainWindow::on_label_logo_linkActivated(const QString &link)
+{
+    //Página principal
+    ui->stackedWidget_principal->setCurrentIndex(0);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,11 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_verRegistros_clicked();
+
+    void on_label_logo_linkActivated(const QString &link);
+
 private:
     Ui::MainWindow *ui;
 };
