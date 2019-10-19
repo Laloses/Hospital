a335cc9bcbd18a889c2c1372aecbc00b35fc456f
abrahamRRA96 <55512300+abrahamRRA96@users.noreply.github.com>
2019-10-19 12:59:33 -0500

    Add files via upload

---
+++ testconsulta.cpp
@@ -0,0 +1,14 @@
+#include "testconsulta.h"
+#include <QtTest>
+#include<QtTest/QtTest>
+#include <QCoreApplication>
+#include <QtSql/QSqlQuery>
+#include <QTestEventList>
+#include <QtSql/QSqlDatabase>
+#include <QDebug>
+#include <QtDebug>
+#include <QtWidgets/QLineEdit>
+testconsulta::testconsulta()
+{
+
+}
