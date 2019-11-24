74a671d8546d939f140a895bec40ae09633f63cf
laloses <laloses_eduardo@hotmail.com>
2019-11-24 13:05:43 -0600

    login-paciente-doctor

--- mainwindow.h
+++ mainwindow.h
@@ -240,8 +240,6 @@ private slots:
 
     void on_pb_adminServicios_clicked();
 
-    void on_pushButton_4_clicked();
-
     public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
